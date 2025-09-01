7d7ef9158b3b15e84ffce8a384306b2d617a69b8
Giorgio Caculli <giorgio.caculli@icloud.com>
2025-09-01 12:31:38 +0300

    Initial Commit

---
+++ Sokoban/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Sokoban
+//
+//  Created by Giorgio Caculli on 01/09/2025.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ SokobanTests/SokobanTests.swift
@@ -0,0 +1,17 @@
+//
+//  SokobanTests.swift
+//  SokobanTests
+//
+//  Created by Giorgio Caculli on 01/09/2025.
+//
+
+import Testing
+@testable import Sokoban
+
+struct SokobanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
